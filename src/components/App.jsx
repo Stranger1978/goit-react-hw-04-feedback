8ee03ca4cbc7ce3208e189fb87c9a628ef5a7f7d
Pavlo Yeromenko <108014692+Stranger1978@users.noreply.github.com>
2023-08-29 14:31:25 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,60 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import FeedBackOptions from './FeedBackOptions';
 import Statistics from './Statistics';
 import Section from './Section';
 import Notification from './Notification';
 
-export class App extends Component { 
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-    };
+export  function App () {
+  const [counterGood, setCounterGood] = useState(0);
+  const [counterNeutral, setCounterNeutral] = useState(0);
+  const [counterBad, setCounterBad] = useState(0);
 
-  incrementFeedback = (buttonName) => {
-    this.setState(prevState => ({
-      [buttonName]: prevState[buttonName] + 1,
-    }));
+  const totalCountFeedback = () => {
+    const totalCount = counterGood + counterNeutral + counterBad;
+    return totalCount;
   };
 
-  countTotalFeedback = () => {
-        const { good, neutral, bad } = this.state;
-        return good + neutral + bad;
-    };
-    
-  countPositiveFeedbackPercentage = () => {
-        const { good } = this.state;
-        const TotalCountFeedback = this.countTotalFeedback();
-        return TotalCountFeedback ? Math.ceil((good / TotalCountFeedback) * 100) : 0;  
-     };
+  const incrementFeedback = e => {
+    const buttonName = e.target.textContent.toLowerCase();
+    switch (buttonName) {
+      case 'good':
+        setCounterGood(state => state + 1);
+        break;
+      case 'neutral':
+        setCounterNeutral(state => state + 1);
+        break;
+      case 'bad':
+        setCounterBad(state => state + 1);
+        break;
+      default:
+        break;
+    }
+  };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const TotalFeedback = this.countTotalFeedback();
-        return (
+  const countPositiveFeedbackPercentage = () => {
+    const PositiveFeedbackPercentage = Math.ceil((counterGood / totalCountFeedback) * 100);
+    return PositiveFeedbackPercentage ? PositiveFeedbackPercentage : 0;
+  } 
+  
+  return (
           <div class="container">
             <Section title="Please leave feedback">
                 <FeedBackOptions
                   options={['good', 'neutral', 'bad']}
-                  onLeaveFeedback={this.incrementFeedback}
+                  onLeaveFeedback={incrementFeedback}
                 />
             </Section>
             <Section title="Statistics">
-              {TotalFeedback ? <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={TotalFeedback}
-                positivePercentage={this.countPositiveFeedbackPercentage()}
+              {totalCountFeedback ? <Statistics
+                good={counterGood}
+                neutral={counterNeutral}
+                bad={counterBad}
+                total={totalCountFeedback}
+                positivePercentage={countPositiveFeedbackPercentage}
               /> :
                 <Notification message="There is no feedback"></Notification>
               } 
             </Section>
           </div>
         )
-  };
 };
